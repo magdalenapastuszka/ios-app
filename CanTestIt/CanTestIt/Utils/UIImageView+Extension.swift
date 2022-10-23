c79ce695a2a9d0bddf40b58f4eaf796eabf6051c
Piotr Bogus <piotr.bogus@finanteq.com>
2022-10-23 09:19:53 +0200

    fixed image downloading

--- CanTestIt/CanTestIt/Utils/UIImageView+Extension.swift
+++ CanTestIt/CanTestIt/Utils/UIImageView+Extension.swift
@@ -3,11 +3,19 @@ import Kingfisher
 
 extension UIImageView {
     func handleImage(with name: String?) {
-        guard let name = name else { return }
-        let url = URL(string: AppVariables.baseURL + "images/" + name)
+        let url = URL(string: AppVariables.baseURL + "images/" + (name ?? "empty"))
         DispatchQueue.main.async { [weak self] in
             self?.kf.indicatorType = .activity
-            self?.kf.setImage(with: url, placeholder: UIImage.emptyImage)
+            self?.kf.setImage(with: url) { result in
+                switch result {
+                case .failure(_):
+                    self?.image = .emptyImage
+                    self?.contentMode = .center
+                case .success(let imageResult):
+                    self?.image = imageResult.image
+                    self?.contentMode = .scaleAspectFill
+                }
+            }
         }
     }
 }
